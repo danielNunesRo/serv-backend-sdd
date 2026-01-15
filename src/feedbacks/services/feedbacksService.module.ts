2da4777a64c817408a0fe5ef6a02ccd59e52c2ac
Daniel Nunes <danielnunespk@gmail.com>
2026-01-15 14:24:06 -0300

    feat: add route getUsersFeedback

--- src/feedbacks/services/feedbacksService.module.ts
+++ src/feedbacks/services/feedbacksService.module.ts
@@ -3,10 +3,15 @@ import { DatabaseModule } from 'src/database/database.module';
 import { PostFeedbackService } from './postFeedback/service/postFeedback.service';
 import { PostFeedbackRepository } from './postFeedback/repositories/postFeedback.repository';
 import { FeedbackController } from '../controller/feedback.controller';
+import { GetUsersFeedbackService } from './getUsersFeedback/service/getUsersFeedback.service';
+import { GetUsersFeedbackRepository } from './getUsersFeedback/repositories/getUserFeedback.repository';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [FeedbackController],
-    providers: [PostFeedbackService,PostFeedbackRepository]
+    providers: [PostFeedbackService,
+        PostFeedbackRepository, 
+        GetUsersFeedbackService, 
+        GetUsersFeedbackRepository]
 })
 export class FeedbacksServicesModule {}
